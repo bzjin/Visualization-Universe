01a2c7173e21dfa192c99267970250fb15671ab8
bzjin <beatricezjin@gmail.com>
2017-05-11 09:05:51 -0400

    sorting
    transitions not working yet

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,20 @@
 $.getJSON("data/sample.json", function(data) {
-    
+	data.charts.sort(function(a,b) { 
+	    var nameA = a.name;
+	    var nameB = b.name; 
+	    if (nameA < nameB) { return -1; }
+	    if (nameA > nameB) { return 1; }
+	    return 0; 
+	})
+	data = data; 
+    makeGrids (data);
+})
+
+var tf_alpha = 0;
+var tf_vol = 0;
+var tf_delta = 0; 
+
+function makeGrids(data){
 	for (i=0; i<6; i++){
 		var div = document.createElement('div');
 		div.setAttribute('class', 'col-lg-2 col-md-3 col-sm-4 col-xs-6'); 
@@ -10,7 +25,43 @@ $.getJSON("data/sample.json", function(data) {
 		$("div#b"+i).append("<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow=" + data.charts[i]["average-popularity"] +
 		  "aria-valuemin='0' aria-valuemax='100' style='width:" + data.charts[i]["average-popularity"] + "%''><span class='sr-only'>70% Complete</span></div></div>");
 	}
-});
+
+	d3.select("#alphabetize").on("click", function(){
+		 $('.col-lg-2').addClass("bye");
+		 //$('.bye').addClass('transform-active');
+		olddata = data; 
+
+		if (tf_alpha == 0) {
+			data.charts.sort(function(a,b) { 
+			    var nameA = a.name;
+			    var nameB = b.name; 
+			    if (nameA < nameB) { return -1; }
+			    if (nameA > nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_alpha = 1; 
+		} else if (tf_alpha == 1) {
+			data.charts.sort(function(a, b) { 
+			    var nameA = a.name;
+			    var nameB = b.name; 
+			    if (nameA > nameB) { return -1; }
+			    if (nameA < nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_alpha = 0; 
+		}
+
+		data.charts.forEach(function(d){
+			var num = data.charts.indexOf(d); 
+			$('#b' + num).animate({
+	           left: $('#b' + data.charts.indexOf(d)).offset().left-$("body").offset().left
+	       });
+		})
+
+
+})
+
+}
 
 $(function() {
     $.ajax({
@@ -35,16 +86,3 @@ $(function() {
 
 
 
-      /*
-'use strict';
-
-var util = require('util');
-var googleTrends = require('../lib/google-trends-api.min.js');
-
-googleTrends.interestOverTime({keyword: 'bar chart', startTime: new Date(Date.now() - (5 * 365 * 24 * 60 * 60 * 1000))})
-.then(function(results){
-  console.log('These results are awesome', results);
-})
-.catch(function(err){
-  console.error('Oh no there was an error', err);
-});*/
