5d0710b61d5c1a0c7c7bd9f6159293bb2d3f8f01
bzjin <beatricezjin@gmail.com>
2017-05-12 21:10:58 -0400

    icons, sorting, grid styling
    grid expands with added divs, icons appended, sorting for name and
    search volume working.

--- javascript/app.js
+++ javascript/app.js
@@ -6,30 +6,33 @@ $.getJSON("data/sample.json", function(data) {
 	    if (nameA > nameB) { return 1; }
 	    return 0; 
 	})
-	data = data; 
     makeGrids (data);
 })
 
-var tf_alpha = 0;
+var tf_alpha = 1;
 var tf_vol = 0;
 var tf_delta = 0; 
 
 function makeGrids(data){
-	for (i=0; i<6; i++){
+	for (i=0; i< data.charts.length; i++){
 		var div = document.createElement('div');
-		div.setAttribute('class', 'col-lg-2 col-md-3 col-sm-4 col-xs-6'); 
+		div.setAttribute('class', 'col-lg-2 col-md-3 col-sm-4 col-xs-6 chartgrids'); 
 		div.setAttribute('id', 'b' + i); 
-		document.getElementById("charts_boxes").appendChild(div);
-		console.log(data.charts[i].name)
-		$("div#b"+i).text(data.charts[i].name);
+		document.getElementById("charts").appendChild(div);
+		//console.log(data.charts[i].name)
+		$("div#b"+i).append("<img id='bimg"+i+"' src='assets/icons/pie-chart.png'>")
+		$("div#b"+i).append("<p>"+data.charts[i].name+"</p>");
 		$("div#b"+i).append("<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow=" + data.charts[i]["average-popularity"] +
 		  "aria-valuemin='0' aria-valuemax='100' style='width:" + data.charts[i]["average-popularity"] + "%''><span class='sr-only'>70% Complete</span></div></div>");
 	}
+	var w = $("#b1").width();
+	var h = $("#b1").height();
+	var pos = $("#b1").position();
 
 	d3.select("#alphabetize").on("click", function(){
-		 $('.col-lg-2').addClass("bye");
-		 //$('.bye').addClass('transform-active');
-		olddata = data; 
+		d3.selectAll(".chartgrids").transition()
+			.style("opacity",0)
+			.remove()
 
 		if (tf_alpha == 0) {
 			data.charts.sort(function(a,b) { 
@@ -51,15 +54,64 @@ function makeGrids(data){
 			tf_alpha = 0; 
 		}
 
-		data.charts.forEach(function(d){
-			var num = data.charts.indexOf(d); 
-			$('#b' + num).animate({
-	           left: $('#b' + data.charts.indexOf(d)).offset().left-$("body").offset().left
-	       });
-		})
+	makeGrids(data)
+	})
 
+	d3.select("#volume").on("click", function(){
+		d3.selectAll(".chartgrids").transition()
+			.style("opacity",0)
+			.remove()
 
-})
+		if (tf_volume == 0) {
+			data.charts.sort(function(a,b) { 
+			    var nameA = a.name;
+			    var nameB = b.name; 
+			    if (nameA < nameB) { return -1; }
+			    if (nameA > nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_volume = 1; 
+		} else if (tf_alpha == 1) {
+			data.charts.sort(function(a, b) { 
+			    var nameA = a.name;
+			    var nameB = b.name; 
+			    if (nameA > nameB) { return -1; }
+			    if (nameA < nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_volume = 0; 
+		}
+
+	makeGrids(data)
+	})
+
+	d3.select("#delta").on("click", function(){
+		d3.selectAll(".chartgrids").transition()
+			.style("opacity",0)
+			.remove()
+
+		if (tf_delta == 0) {
+			data.charts.sort(function(a,b) { 
+			    var nameA = a["popularity-delta"];
+			    var nameB = b["popularity-delta"]; 
+			    if (nameA < nameB) { return -1; }
+			    if (nameA > nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_delta = 1; 
+		} else if (tf_alpha == 1) {
+			data.charts.sort(function(a, b) { 
+			    var nameA = a["popularity-delta"];;
+			    var nameB = b["popularity-delta"];; 
+			    if (nameA > nameB) { return -1; }
+			    if (nameA < nameB) { return 1; }
+			    return 0; 
+			  })
+			tf_delta = 0; 
+		}
+
+	makeGrids(data)
+	})
 
 }
 
